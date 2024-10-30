39f1edfe81258eea93302e97f2e875f6c99a8504
WillsWebsites <contactwillswebsites@gmail.com>
2024-10-29 23:07:46 -0600

    adding some images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Container from "@/components/ui/Container";
 import ReservationForm from "@/components/pages/Home/ReservationForm";
 import Details from "@/components/Details";
-
+import Image from "next/image";
 export default function HomePage() {
   return (
     <section className="flex min-h-[100vh] bg-gradient-to-r from-slate-800 to-slate-700">
@@ -11,6 +11,10 @@ export default function HomePage() {
           <p>Wedding reception invitation and RSVP</p>
           <Details />
           <ReservationForm />
+        </div>
+        <div>
+          <Image src="/1.jpeg" alt="Will & Mia" width={500} height={500} />
+          <Image src="/3.JPG" alt="Will & Mia" width={500} height={500} />
         </div>
       </Container>
     </section>
